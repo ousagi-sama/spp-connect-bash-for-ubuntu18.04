7fb463d4221ae90926ea80de58f215c9c1ea28aa
ousagi-sama <omusagi@gmail.com>
2021-09-09 15:19:40 +0900

    Create spp-connect

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # spp-connect-bash-for-ubuntu18.04
+
+Ubuntu18.04の環境にて、\
+GPSモジュール<sup>[1](#note1)</sup>Bluetooth SPP接続を行うためのScript  
+
+容易に接続するためのBash Scriptを書いた.
+
+## 使い方
+
+接続するための仮想ポートを用意する．  
+- ${ID} : 仮想ポートのID、基本的には0でよい  
+- ${ADDRESS} : 接続するデバイスアドレス
+- ${BAUDRATE} : ボーレート
+
+```
+bash spp-connect.sh ${ID} ${ADDRESS} ${BAUDRATE}
+```
+
+一度通信行えば.spp-connect-configにその情報が保存されるため、  
+次回からは、そのまま行えば良い
+
+```
+bash spp-connect.sh
+```
+
+![](test.png)
+
+***
+
+<div id="note1">1 : https://www.bizstation.jp/ja/drogger/dg-pro1rws_index.html</div>
---
+++ spp-connect.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+#使い方:#
+#    spp-connect <[id] [address] [baudrate]>#
+
+if [ $# -eq 3 ];then
+    id=$1; address=$2; baudrate=$3
+    echo "$id $address $baudrate" > .spp-connect-config
+elif [ $# -eq 0 ];then
+    set $(cat .spp-connect-config)
+    id=${1}; address=${2}; baudrate=${3}
+else
+    text=$(cat $0 | grep "^#.*#$")
+    echo -e "\033[33m${text//#}\033[32m"
+    exit 1
+fi
+
+# 仮想ポートの作成
+sudo rfcomm bind $id $address
+sudo chmod 777 /dev/rfcomm$id
+sudo stty -F /dev/rfcomm$id $baudrate cs8
+
+read -p "Press [Enter] key to resume."
+
+# 仮想ポートでの解除
+sudo rfcomm release $id
